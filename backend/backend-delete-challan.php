85d562b9b4d7fb0cc65e9f3bcb19726d1e335738
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-05-04 21:34:04 +0545

    Delete Challan Added
    Added the backend functionality of delete challan and connected with traffic and admin view page.

---
+++ backend/backend-delete-challan.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: ../frontend/index.html");
+}
+?>
+<?php
+
+include "database_connect.php";
+
+$challan_id = $_POST['id'];
+
+// Attempt update query execution
+$sql = "DELETE from challan WHERE challan_id=$challan_id";
+
+if ($con->query($sql) === true) {
+
+    if(isset($_GET['from'])&&$_GET['from']=='admin'){
+        header("Location: ../frontend/admin-view-challan.php?success=Successfully Deleted");
+
+    }else{
+        header("Location: ../frontend/traffic-view-challan.php?success=Successfully Deleted Challan");
+
+    }
+
+} else {
+    if(isset($_GET['from'])&&$_GET['from']=='admin'){
+        header("Location: ../frontend/admin-view-challan.php?success=Error while deleting challan");
+
+    }else{
+        header("Location: ../frontend/traffic-view-challan.php?error=Error while deleting challan");
+
+    }
+
+}
+
+// Close connection
+$con->close();
+
+
+
